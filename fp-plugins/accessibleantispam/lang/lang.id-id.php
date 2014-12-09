447b101ceab8e35d8a6ea2c5ee4e2dec1007c957
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:25:20 +0700

    Create lang.id-id.php

---
+++ fp-plugins/accessibleantispam/lang/lang.id-id.php
@@ -0,0 +1,22 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+$lang['plugin']['accessibleantispam'] = array(
+		
+	'prefix'	=> 'Pertanyaan mudah agar tidak dianggap spam, '.
+		'Hasil dari ',
+		
+	'sum'		=> '%s + %s ?',
+	'sub'		=> '%1$s - %2$s ?',
+	'prod'		=> '%s * %s ?',
+		
+	'error'		=> 'Maaf, Jawaban Anda Salah, Silahkan Ulangi Kembali.'
+	
+	);
+
