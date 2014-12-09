ba432c20f41855680a317153b18e712df0262f44
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:31:23 +0700

    Create lang.id-id.php

---
+++ fp-plugins/lightbox2/lang/lang.id-id.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['plugin']['lightbox']['errors'] = array (
+		-1	=> 'Plugin ini memerlukan jsUtils untuk berkerja. Mungkin plugin telah dimuat dalam urutan yang salah'
+	);
+?>
