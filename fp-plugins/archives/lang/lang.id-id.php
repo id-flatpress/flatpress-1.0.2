c091cefffedf4f1d3df5d3d6d3b1bd4f142eab6f
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:27:09 +0700

    Create lang.id-id.php

---
+++ fp-plugins/archives/lang/lang.id-id.php
@@ -0,0 +1,17 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+	$lang['plugin']['archives'] = array(
+		
+		'subject'			=> 'Arsip',
+		'no_posts'		=> 'Belum ada Tulisan',
+	
+	);
+
+?>
