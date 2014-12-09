52c5820db5326281f12400f574a98fa5267ff6b3
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:19:59 +0700

    Create lang.admin.uploader.php

---
+++ fp-interfaces/lang/id-id/lang.admin.uploader.php
@@ -0,0 +1,35 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+	$lang['admin']['uploader']['default'] = array(
+		'head'		=> 'Pengunggahan',
+		'descr'		=> 'Silahkan pilih File untuk di unggah.',
+		'fset1'		=> 'Temukan File',
+		'fset2'		=> 'Unggah',
+		'submit'	=> 'Unggah',
+
+	);
+
+	$lang['admin']['uploader']['default']['msgs'] = array(
+		1	=> 'File telah di unggah :)',
+		-1	=> 'Tampaknya ada kesalahan, silahkan Anda coba kembali:(',
+	);
+	
+	
+	
+	$lang['admin']['uploader']['browse'] = array(
+		'head'		=> 'Cari',
+		'descr'		=> 'Ambil satu atau lebih file untuk di unggah.',
+		'fset1'		=> 'File Dipilih',
+		'submit'	=> 'Unggah',
+
+	);
+
+	
+?>
