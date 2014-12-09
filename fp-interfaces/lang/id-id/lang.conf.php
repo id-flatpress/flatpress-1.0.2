4e6abe28cb6b659b2ff53f4589c7910e6ec6b7b5
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:20:48 +0700

    Create lang.conf.php

---
+++ fp-interfaces/lang/id-id/lang.conf.php
@@ -0,0 +1,15 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$langconf = array();
+	$langconf['id'] = 'Bahasa Indonesia';
+	$langconf['locale'] = 'id-id';
+	$langconf['charsets'][0] = 'utf-8';
+	$langconf['charsets'][1] = 'iso-8859-15';
+
+?>
