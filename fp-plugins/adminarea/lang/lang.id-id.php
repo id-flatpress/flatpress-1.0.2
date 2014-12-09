c04805024593f610c324cdc0102a2e5b2be63d7d
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:25:55 +0700

    Create lang.id-id.php

---
+++ fp-plugins/adminarea/lang/lang.id-id.php
@@ -0,0 +1,21 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+	$lang['plugin']['adminarea'] = array(
+		
+		'subject'		=> 'Area Admin',
+		'welcome'		=> 'Haloo ',
+		'admin_panel'	=> 'Admin panel',
+		'add_entry'		=> 'Tambah entri baru',
+		'add_static'	=> 'Buat halaman',
+		'logout'		=> 'Keluar'
+	
+	);
+
+?>
