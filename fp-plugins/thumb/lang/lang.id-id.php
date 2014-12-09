0558b729dbb4990bd57b45cea8e47f6ddc7333ce
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:34:23 +0700

    Create lang.id-id.php

---
+++ fp-plugins/thumb/lang/lang.id-id.php
@@ -0,0 +1,15 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['plugin']['thumb']['errors'] = array (
+		-1	=> 'Galat: Tidak ditemukkan '.
+			'<a href="http://www.php.net/manual/en/ref.image.php">PHP ekstensi GD2</a> ' .
+			'dibutuhkan <strong>thumb</strong> untuk bekerja. Silahkan menonaktifkan dari panel ' .
+			'untuk mengaktifkan ekstensi ini'
+	);
+?>
