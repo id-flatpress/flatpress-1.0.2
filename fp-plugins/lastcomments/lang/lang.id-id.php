293ac888a47f57aaed0a22f40cbb39763ac65ec6
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:30:07 +0700

    Create lang.id-id.php

---
+++ fp-plugins/lastcomments/lang/lang.id-id.php
@@ -0,0 +1,18 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['plugin']['lastcomments'] = array(
+		
+		'last'				=> '',
+		'comments'			=> 'Komentar terakhir',
+		'no_comments'		=> 'Tidak ada komentar',
+		'no_new_comments'	=> 'Tidak ada komentar baru'
+		
+	);
+
+?>
