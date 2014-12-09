cf4ac7823256dab8b337eb3cc9169fab9c18a6cc
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:29:42 +0700

    Create lang.id-id.php

---
+++ fp-plugins/categories/lang/lang.id-id.php
@@ -0,0 +1,16 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+	$lang['plugin']['categories'] = array(
+		
+		'subject'	=> 'Kategori'
+	
+	);
+
+?>
