75b873c7963a8bc09b9256dbf0cec7ad95f9aaa1
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:19:44 +0700

    Create lang.admin.themes.php

---
+++ fp-interfaces/lang/id-id/lang.admin.themes.php
@@ -0,0 +1,54 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	
+	$lang['admin']['themes']['submenu'] = array(
+		'default'	=> 'Tema',
+		'style'		=> 'Gaya',
+	);
+	
+	/* default panel action (change theme) */
+	
+	$lang['admin']['themes']['default'] = array(
+		'head1'		=> 'Tema Saat ini',
+		
+		'head2'		=> 'Tema yang tersedia',
+		'descr'		=> 'Pilih tema untuk mengaktifkan',
+		
+		'nodescr'	=> 'Tidak ada keterangan',
+		'noauthor'	=> 'Pembuat tidak diketahui',
+
+	);
+	
+	$lang['admin']['themes']['default']['msgs'] = array(
+		1 => 'Tema baru telah diaktifkan',
+		-1 => 'Tampaknya ada kesalahan sehingga tidak tersimpan, silahkan ulangi kembali.',
+		-2 => 'Tampaknya tema yang Anda pilih tidak tersedia.'
+	);
+	
+	
+	/* select style */
+	
+	$lang['admin']['themes']['style'] = array(
+		'head1'		=> 'Gaya Saat ini',
+		
+		'head2'		=> 'Gaya yang Tersedia',
+		'descr'		=> 'Pilih untuk meng-aktifkan',
+		
+		'nodescr'	=> 'Tidak ada keterangan',
+		'noauthor'	=> 'Tidak diketahui',
+	);
+	
+	$lang['admin']['themes']['style']['msgs'] = array(
+		1 => 'Style berhasil disimpan',
+		-1 => 'Tampaknya ada kesalahan sehingga tidak tersimpan, silahkan ulangi kembali..',
+		-2 => 'Tampaknya tema yang Anda pilih tidak tersedia.'
+	);
+	
+	
+?>
