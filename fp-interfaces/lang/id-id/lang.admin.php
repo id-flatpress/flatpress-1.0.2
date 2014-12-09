76284068928eb8833bebaad735b492f688a4f626
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:18:47 +0700

    Create lang.admin.php

---
+++ fp-interfaces/lang/id-id/lang.admin.php
@@ -0,0 +1,40 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['admin']['head'] = 'Area Admin';
+	
+	$lang['admin']['panels'] = array(
+		'main'		=> 'Pilihan',
+		'config'	=> 'Pengaturan',
+		'users'		=> 'Pengguna',
+		'entry'		=> 'Tulisan',
+		'static'	=> 'Statis',
+		'uploader'	=> 'Unggah',
+		'widgets'	=> 'Widget',
+		'add'		=> 'Tambah',
+		'edit'		=> 'Ubah/Hapus',
+		'maintain'	=> 'Pelihara',
+		'plugin'	=> 'Plugin',
+		'themes'	=> 'Tema',
+		'updates'	=> 'Pembaruan',
+	);
+	
+	
+	$lang['admin']['general'] = array(
+		'id'		=> 'Id',	# for entry ids
+		'name'		=> 'Nama', 	# for file names
+		'title'		=> 'Judul', # for entry/posts/static titles
+		'comments'	=> 'Komentar',
+		'author'	=> 'Penulis',
+		'actdel'	=> 'Hapus',
+		'actedit'	=> 'Ubah',
+		'actenable'	=> 'Aktifkan',
+		'actdisable'=> 'Nonaktifkan',
+	);
+
+?>
