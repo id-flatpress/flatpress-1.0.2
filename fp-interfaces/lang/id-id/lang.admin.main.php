edf3b9b23f46b9118529c8f5fad4caaaa5d8800f
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:18:02 +0700

    Create lang.admin.main.php

---
+++ fp-interfaces/lang/id-id/lang.admin.main.php
@@ -0,0 +1,29 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['admin']['panel']['main'] = 'Pilihan';
+	
+	$lang['admin']['main']['default'] = array(
+		'head'		=> 'Selamat datang di admin panel!',
+		'descr'		=> 'Apa yang ingin Anda lakukan?',
+		
+		'op1'		=> 'Tulisan Baru',
+		'op1d'		=> 'Buat Tulisan Entri Baru',
+		'op2'		=> 'Tulisan',
+		'op2d'		=> 'Lihat dan atur tulisan entri Anda',
+		'op3'		=> 'Widget',
+		'op3d'		=> 'Atur dan Sesuaikan Widget samping, atas dan bawah',
+		'op4'		=> 'Plugin',
+		'op4d'		=> 'Lihat, konfigurasi, aktifkan atau nonaktifkan plugin',
+		'op5'		=> 'Pengaturan',
+		'op5d'		=> 'Pengaturan Blog FlatPress Anda',
+		'op6'		=> 'Pemeliharaan',
+		'op6d'		=> 'Temukan masalah, dan kembalikan pengaturan awal FlatPress',
+		
+	);
+?>
