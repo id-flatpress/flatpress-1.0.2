f4d1798d9170192e8a91a68f5551eb9754f7730b
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:21:02 +0700

    Create lang.contact.php

---
+++ fp-interfaces/lang/id-id/lang.contact.php
@@ -0,0 +1,37 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://eg.goez.tk
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['contact'] = array(
+		'head'		=> 'Kontak',
+		'descr'		=> 'Silahkan isi formulir di bawah ini untuk mengirimkan pesan, tambahkan email anda jika anda ingin balasan :)',
+		'fieldset1'	=> 'Informasi yang dibutuhkan',
+		'name'		=> 'Nama (*)',
+		'email'		=> 'Email:',
+		'www'		=> 'Website:',
+		'cookie'	=> 'Ingat saya',
+		'fieldset2'	=> 'Konten',
+		'comment'	=> 'Pesan (*):',
+		'fieldset3'	=> 'Kirim',
+		'submit'	=> 'Kirim',
+		'reset'		=> 'Bersihkan',
+		
+	);
+	
+	$lang['contact']['error'] = array(
+		'name'		=> 'Anda harus masukkan nama',
+		'email'		=> 'Anda harus masukkan e-mail yang benar',
+		'www'		=> 'Masukan URL yang benar',
+		'content'	=> 'Masukan sebuah konten',
+	);
+	
+	$lang['contact']['msgs'] = array(
+		1	=> 'Terima Kasih, Pesan Anda sudah terkirim :)',
+		-1	=> 'Maaf Pesan Anda gagal dikirim',
+	);
+	
+?>
