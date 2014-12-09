1ca21d9eaaf416e94f53cfe0108e38eb3c78dfab
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:30:38 +0700

    Create lang.id-id.php

---
+++ fp-plugins/lastentries/lang/lang.id-id.php
@@ -0,0 +1,20 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['plugin']['lastentries'] = array(
+		
+		'subject_before_count'	=> '',
+		'subject_after_count'	=> ' Tulisan terakhir',
+		'edit'			=> 'Sesuaikan',
+		'add_entry'		=> 'Tambah entri',
+		'no_entries'	=> 'Tidak ada entri'
+	
+	
+	);
+
+?>
