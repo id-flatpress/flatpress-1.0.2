7761a943cdaafd0463862a58aabc52474a1a2f22
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:20:31 +0700

    Create lang.comments.php

---
+++ fp-interfaces/lang/id-id/lang.comments.php
@@ -0,0 +1,27 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['comments']['mail'] = <<<MAIL
+Haloo %toname%,
+
+"%fromname%" %frommail% telah menambahkan sebuah komentar pada entri "%entrytitle%".
+
+Link komentar:
+%commentlink%
+
+Isi Komentar:
+***************
+%content%
+***************
+
+Salam Hangat..
+%blogtitle%
+
+MAIL;
+
+?>
