cc29ab8241c521392378bda4c9b535195154113b
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:28:29 +0700

    Create lang.id-id.php

---
+++ fp-plugins/blockparser/lang/lang.id-id.php
@@ -0,0 +1,33 @@
+<?php
+/*
+LangId: Bahasa Indonesia translate by: eggoez - http://eggoez.com
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# mohon hubungi saya di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+	$lang['admin']['widgets']['submenu']['blockparser'] = 'BlockParser Widget';
+	
+	$lang['admin']['widgets']['blockparser'] = array(
+		'head'		=> 'Widget BlockParser',
+		'description'	=> 'Plugin BlockParser, memungkinkan Anda membuat widget dari sebuah halaman statis. </p><p>
+		Pilih satu atau lebih banyak halaman statis dari daftar untuk menampilkan dan mennyembunyikan pada widget.</p><p>
+		Setiap  <a href="?p=static&amp;action=write">halaman statis baru</a> yang Anda buat, akan otomatis berada di sini.',
+
+		'id'		=> 'Halaman statis',
+		'title'		=> 'Judul',
+		'action'	=> 'Aksi',
+		'enable'	=> 'Aktifkan',
+		'disable'	=> 'Nonaktifkan',
+		'edit'		=> 'Sunting',
+		
+	);
+	$lang['admin']['widgets']['blockparser']['msgs'] = array(
+		1		=> 'Widget Sudah Tersedia, Tambahkan kedalam Sidebar Widget pada <a href="?p=widgets">main panel!</a>',
+		-1		=> 'Tidak bisa membuat permintaan widget',
+		2		=> 'Berhasil dinonaktifkan, jangan lupa untuk menghapus tatanan widget dari <a href="?p=widgets">main panel</a>!',
+		-2		=> 'Kesalahan ketika menonaktifkan widget'
+	);
+	
+?>
